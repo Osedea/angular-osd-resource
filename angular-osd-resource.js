8352bf0cc7ab2af947cc4366e2701fb291cb670d
Alexandre Murphy-Gonthier <murphyalexandre@gmail.com>
2015-05-29 08:49:16 -0400

    Cache decorator now caches multiples calls depending on params.

--- angular-osd-resource.js
+++ angular-osd-resource.js
@@ -183,21 +183,51 @@
             'delete'
         ];
 
+        var forced = false;
+
         // Give the decorator all methods that the delegated resource has
         lodash.extend(decorator, $delegate);
 
         // Add relation resources to the list of cached calls.
         cachedCalls = cachedCalls.concat($delegate.config.relations);
 
+        function objectToString(obj) {
+            var result = [];
+
+            lodash.forIn(obj, function (value, key) {
+                result.push(key + ':' + value);
+            });
+
+            return result.join('&');
+        }
+
+        /*
+         Simple hash function to convert string to integer. This is the java implementation of a
+         hashing algorithm.
+         Source: http://stackoverflow.com/a/7616484
+         */
+        function hash(str) {
+            var res = 0;
+            var len = str.length;
+
+            for (var i = 0; i < len; i++) {
+                res = res * 31 + str.charCodeAt(i);
+            }
+
+            return res;
+        }
+
         /*
          Create empty caches for a specific resource. Each resource method has its own cache object
          which keeps track of the cached data, the query params used and whether or data has been cached.
          */
-        function initResourceCache() {
+        function initResourceCache(hash) {
             self.caches[$delegate.config.name] = {};
 
             lodash.forEach(cachedCalls, function (call) {
-                self.caches[$delegate.config.name][call] = {
+                self.caches[$delegate.config.name][call] = {};
+
+                self.caches[$delegate.config.name][call][hash] = {
                     cached: false,
                     params: null,
                     data: null
@@ -211,23 +241,26 @@
          If not forced, cached is true, and the query params are the same,
          return the cached data; otherwise make the API call.
          */
-        function makeCachedCall(call, params, forced) {
+        function makeCachedCall(call, params) {
             var currentCache = self.caches[$delegate.config.name];
+            var paramsHash = hash(objectToString(params));
 
-            if (!currentCache) {
-                currentCache = initResourceCache();
+            if (!currentCache || !currentCache[call] || !currentCache[call][paramsHash]) {
+                currentCache = initResourceCache(paramsHash);
             }
 
-            if (!forced && currentCache[call].cached && lodash.isEqual(params, currentCache[call].params)) {
-                return currentCache[call].data;
+            // Check if we explicitely don't want a cached call or if it's
+            // already cached.
+            if (!forced && currentCache[call][paramsHash].cached) {
+                return currentCache[call][paramsHash].data;
             }
 
             /*
              Here we cache the results and set flags for the next
              time the resource method is called.
              */
-            currentCache[call].cached = true;
-            currentCache[call].params = params;
+            currentCache[call][paramsHash].cached = true;
+            currentCache[call][paramsHash].params = params;
 
             /*
              This is the decorated call.
@@ -237,7 +270,10 @@
                     return response;
                 });
 
-            currentCache[call].data = promisedResponse;
+            currentCache[call][paramsHash].data = promisedResponse;
+
+            // Reset forced flag
+            forced = false;
 
             return promisedResponse;
         }
@@ -253,16 +289,28 @@
                 currentCache = initResourceCache();
             }
 
-            currentCache.get.cached = false;
-            currentCache.query.cached = false;
+            lodash.forEach(currentCache.get, function (cacheCall) {
+                cacheCall.cached = false;
+            });
+            lodash.forEach(currentCache.query, function (cacheCall) {
+                cacheCall.cached = false;
+            });
 
             return $delegate[call](data);
         }
 
+        // We explicitely want new data. Don't use the cache for the next
+        // request.
+        decorator.setForced = function () {
+            forced = true;
+
+            return decorator;
+        };
+
         // Create decorator methods for all calls that require caching
         lodash.forEach(cachedCalls, function (call) {
-            decorator[call] = function (params, forced) {
-                return makeCachedCall(call, params, forced);
+            decorator[call] = function (params) {
+                return makeCachedCall(call, params);
             };
         });
 
